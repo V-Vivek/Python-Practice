fe672eb17748f5713abcfbb9be3b3f76961cb121
V-Vivek <117569148+V-Vivek@users.noreply.github.com>
2022-11-14 23:14:20 +0530

    Update Functions_Non-Keyed_Arguments.py

--- Functions_Non-Keyed_Arguments.py
+++ Functions_Non-Keyed_Arguments.py
@@ -1,7 +1,7 @@
 # This is a non-keyed argument
-def func1(*vivek):
+def func1(*vivek): # * is used to specify the non-keyed argument
 	'''You can pass any number of arguments to this function'''
 	for v in vivek:
 		print(v)
 
-func1('Hi', 'This', 'is', 'Vivek', 1, 2, 3)
+func1('Hi', 'This', 'is', 'Vivek', 1, 2, 3) # Add any no. of arguments

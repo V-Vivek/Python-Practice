e67302801e741e16c173545390dbd910b8fa9c88
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-20 17:07:08 +0530

    Create Lambda_Functions.py

---
+++ Lambda_Functions.py
@@ -0,0 +1,4 @@
+# This function can have any number of arguments but only one expression, which is evaluated and returned.
+# One is free to use lambda functions wherever function objects are required.
+# You need to keep in your knowledge that lambda functions are syntactically restricted to a single expression.
+# It has various uses in particular fields of programming, besides other types of expressions in functions.

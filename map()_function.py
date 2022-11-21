4dedbf21612fb61fd8bbf5fd5ed025c1e2186376
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-21 21:28:39 +0530

    Update map()_function.py

--- map()_function.py
+++ map()_function.py
@@ -4,8 +4,9 @@
 # Syntax: map(fun, iter)
   # fun : It is a function to which map passes each element of given iterable.
   # iter : It is a iterable which is to be mapped.
-  
-# Write a function to get the square of each item in a list
+
+# ==================================================================================================
+# Problem 1: Write a program to get the square of each item in a list
 
 #Method 1: Without using map()
 lst = [1, 2, 3, 4, 5]
@@ -18,3 +19,21 @@ print(result)
 lst = [1, 2, 3, 4, 5]
 result = map(lambda x : x*x, lst) # map() is used to iterate through each item of list
 print(list(result))
+# ==================================================================================================
+
+# ==================================================================================================
+# Problem 2: Write a function to add sequential elements of two lists
+#Method 1: Without using map()
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [6, 7, 8, 9, 10]
+result =[]
+for i in range(len(lst1)):
+  result.append(lst1[i]+lst2[i])
+print(result)
+  
+#Method 2: Using map()
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [6, 7, 8, 9, 10]
+result = map(lambda x,y : x+y, lst1, lst2) # map() is used to iterate through each item of list
+print(list(result))
+# ==================================================================================================

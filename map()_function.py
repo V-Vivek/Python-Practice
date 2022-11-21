5ad3ba0abfe6fab1f7478f1c579b34f36be57c43
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-21 21:11:00 +0530

    Create map()_function.py

---
+++ map()_function.py
@@ -0,0 +1,6 @@
+# Map is used to apply some logic(function) to each item of an input iterable
+# As a result it produces the map object.
+# NOTE : The returned value from map() (map object) then can be passed to functions like list() (to create a list), set() (to create a set) .
+# Syntax: map(fun, iter)
+  # fun : It is a function to which map passes each element of given iterable.
+  # iter : It is a iterable which is to be mapped.

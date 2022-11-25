03031ab50da14503a4fb6ff1d853fa894aa121a9
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-25 22:49:55 +0530

    Update OOP_Basics.py

--- OOP_Basics.py
+++ OOP_Basics.py
@@ -7,29 +7,34 @@
 # How to create a class in Python
 class Employee:
 	# Class attribute
-    empCount = 0
+	empCount = 0
 	
     # Constructor of class. It is mainly used for assignment of instance variables.
-    def __init__(self, name, salary ):
-        # Instance variable or Instance attributes
-        self.emp_name = name
-        self.emp_salary = salary
-		Employee.empCount += 1  # Incrementing a class attribute for each class object creation
+	def __init__(self, name, salary ):
+	# Instance variable or Instance attributes
+	self.emp_name = name
+	self.emp_salary = salary
+	Employee.empCount += 1  # Incrementing a class attribute for each class object creation
 		
-    # Method of a class - To print instance variables
-    def displayEmployeeInfo(self):
-        print("Employee name : ",self.emp_name, " , Employee Salary : ",self.emp_salary)
+	# Method of a class - To print instance variables
+	def displayEmployeeInfo(self):
+		print("Employee name : ",self.emp_name, " , Employee Salary : ",self.emp_salary)
 		
-	 # Method of a class - To print class variables
-    def displayEmployeeCount(self):
-        print("Employee Count : ",Employee.empCount) # Note thatwe use class name instead of self while accessing Class Variables
+	# Method of a class - To print class variables
+	def displayEmployeeCount(self):
+		print("Employee Count : ",Employee.empCount) # Note thatwe use class name instead of self while accessing Class Variables
 
 # Object instantiation - Creating class objects
 emp1 = Employee('Vivek', 1000)  
 emp2 = Employee('Jadhav', 2000)
 
+# Calling Class methods to print values
 emp1.displayEmployeeInfo()
 emp1.displayEmployeeCount()
 
 emp2.displayEmployeeInfo()
 emp2.displayEmployeeCount()
+
+# Individually accessing & printing the instance variables
+print(emp1.emp_name)
+print(emp2.emp_name)

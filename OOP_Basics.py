5c75a64a88354a0d5af0a5a9dbd74e3bd2ef8e54
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2022-11-25 22:00:53 +0530

    Create OOP_Basics.py

---
+++ OOP_Basics.py
@@ -0,0 +1,35 @@
+# ==================================================================================================================================================================
+# The self keyword:
+# 1. Class methods must have an extra first parameter in the method definition. We do not give a value for this parameter when we call the method, Python provides it
+# 2. If we have a method that takes no arguments, then we still have to have one argument(self).
+# ==================================================================================================================================================================
+
+# How to create a class in Python
+class Employee:
+	# Class attribute
+    empCount = 0
+	
+    # Constructor of class. It is mainly used for assignment of instance variables.
+    def __init__(self, name, salary ):
+        # Instance variable or Instance attributes
+        self.emp_name = name
+        self.emp_salary = salary
+		Employee.empCount += 1  # Incrementing a class attribute for each class object creation
+		
+    # Method of a class - To print instance variables
+    def displayEmployeeInfo(self):
+        print("Employee name : ",self.emp_name, " , Employee Salary : ",self.emp_salary)
+		
+	 # Method of a class - To print class variables
+    def displayEmployeeCount(self):
+        print("Employee Count : ",Employee.empCount) # Note thatwe use class name instead of self while accessing Class Variables
+
+# Object instantiation - Creating class objects
+emp1 = Employee('Vivek', 1000)  
+emp2 = Employee('Jadhav', 2000)
+
+emp1.displayEmployeeInfo()
+emp1.displayEmployeeCount()
+
+emp2.displayEmployeeInfo()
+emp2.displayEmployeeCount()
